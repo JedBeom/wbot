7b52f36f8ad2560f4f07be9364006f569ac7726d
Bombwhale <solkblte@icloud.com>
2019-08-05 21:23:20 +0900

    Fix Event

--- dDay_skill.go
+++ dDay_skill.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/JedBeom/wbot_new/model"
 )
 
 // 디데이 스킬
-func dDaySkill(w http.ResponseWriter, _ *http.Request) {
+func dDaySkill(w http.ResponseWriter, r *http.Request) {
+	history, ok := r.Context().Value("history").(model.History)
+	if !ok {
+		w.WriteHeader(400)
+		return
+	}
 
 	format := `{
 	"version": "2.0",
@@ -29,7 +36,7 @@ func dDaySkill(w http.ResponseWriter, _ *http.Request) {
 	}
 }`
 
-	output := fmt.Sprintf(format, DdayText, payload.BlockID)
+	output := fmt.Sprintf(format, DdayText, history.BlockID)
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	_, err := w.Write([]byte(output))
 	if err != nil {
